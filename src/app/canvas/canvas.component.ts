91562b9d023e20b5bd99104a5d06adfe44d4ae96
Reinhard Langner <ReinhardLangner@gmx.de>
2024-01-20 16:37:31 +0100

    zoom and fix mouse-pointer

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -27,8 +27,13 @@ export class CanvasComponent implements AfterViewInit {
   @Output() canvasHeightChange = new EventEmitter<number>();
 
   @Output() resize = new EventEmitter<DOMRect>();
-  @Output() viewPortOrigin = new EventEmitter<{ x: number; y: number }>();
-  @Output() zoomChange = new EventEmitter<{ deltaX: number; deltaY: number }>();
+  @Output() viewPortChange = new EventEmitter<{ x: number; y: number }>();
+  @Output() zoomChange = new EventEmitter<{
+    deltaX: number;
+    deltaY: number;
+    ptX: number;
+    ptY: number;
+  }>();
 
   @Output() viewPort = new EventEmitter<{
     x: number;
@@ -63,10 +68,10 @@ export class CanvasComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     setTimeout(() => {
-      this.refreshCanvasSize(true);
+      this.refreshCanvasSize();
     });
     window.addEventListener("resize", () => {
-      this.refreshCanvasSize(true);
+      this.refreshCanvasSize();
     });
   }
 
@@ -81,16 +86,20 @@ export class CanvasComponent implements AfterViewInit {
   }
 
   zooming(event: WheelEvent) {
-    this.zoomChange.emit({ deltaX: event.deltaX, deltaY: event.deltaY });
-    // this.zoomViewPort(scale, pt);
+    const pt = this.eventToLocation(event);
+    this.zoomChange.emit({
+      deltaX: event.deltaX,
+      deltaY: event.deltaY,
+      ptX: pt.x,
+      ptY: pt.y,
+    });
   }
 
   panning(event: WheelEvent) {
-    this.viewPortOrigin.emit({
+    this.viewPortChange.emit({
       x: this.viewPortX + event.deltaX / this.zoom,
       y: this.viewPortY + event.deltaY / this.zoom,
     });
-    // this.zoomViewPort(scale, pt);
   }
 
   ngOnInit(): void {
@@ -103,58 +112,34 @@ export class CanvasComponent implements AfterViewInit {
     });
   }
 
-  eventToLocation(
-    event: PointerEvent | TouchEvent,
-    idx = 0
-  ): { x: number; y: number } {
-    const strokeWidth = 1.0;
-
-    const rect = this.canvas.nativeElement.getBoundingClientRect();
-    const touch = event instanceof MouseEvent ? event : event.touches[idx];
-    // let x = this.viewPortX + (touch.clientX - rect.left) * strokeWidth;
-    // let y = this.viewPortY + (touch.clientY - rect.top) * strokeWidth;
-
-    let x = touch.clientX;
-    let y = touch.clientY;
-
-    x = this.viewPortX + x / this.zoom;
-    y = this.viewPortY + y / this.zoom;
-
+  eventToLocation(event: PointerEvent | WheelEvent | TouchEvent) {
+    const touch = event instanceof MouseEvent ? event : event.touches[0];
+    const x = this.viewPortX + touch.clientX / this.zoom;
+    const y = this.viewPortY + touch.clientY / this.zoom;
     return { x, y };
   }
 
-  zoomViewPort(scale: number, pt?: { x: number; y: number }) {
-    if (!pt) {
-      pt = {
-        x: this.viewPortX + 0.5 * this.viewPortWidth,
-        y: this.viewPortY + 0.5 * this.viewPortHeight,
-      };
-    }
-    const w = scale * this.viewPortWidth;
-    const h = scale * this.viewPortHeight;
-    const x =
-      this.viewPortX +
-      (pt.x - this.viewPortX - scale * (pt.x - this.viewPortX));
-    const y =
-      this.viewPortY +
-      (pt.y - this.viewPortY - scale * (pt.y - this.viewPortY));
+  // zoomViewPort(scale: number, pt?: { x: number; y: number }) {
+  //   if (!pt) {
+  //     pt = {
+  //       x: this.viewPortX + 0.5 * this.viewPortWidth,
+  //       y: this.viewPortY + 0.5 * this.viewPortHeight,
+  //     };
+  //   }
+  //   const w = scale * this.viewPortWidth;
+  //   const h = scale * this.viewPortHeight;
+  //   const x =
+  //     this.viewPortX +
+  //     (pt.x - this.viewPortX - scale * (pt.x - this.viewPortX));
+  //   const y =
+  //     this.viewPortY +
+  //     (pt.y - this.viewPortY - scale * (pt.y - this.viewPortY));
 
-    // this.viewPort.emit({ x, y, w, h });
-  }
+  //   // this.viewPort.emit({ x, y, w, h });
+  // }
 
-  refreshCanvasSize(emitEmptyCanvas = false) {
+  refreshCanvasSize() {
     const rect = this.canvas.nativeElement.parentNode.getBoundingClientRect();
-
     this.resize.emit(rect);
-
-    // this.canvasWidth = rect.width;
-    // this.canvasHeight = rect.height;
-
-    // this.viewPort.emit({
-    //   x: this.viewPortX,
-    //   y: this.viewPortY,
-    //   w: this.viewPortWidth,
-    //   h: null,
-    // });
   }
 }
--- src/app/main-editor/main-editor.component.ts
+++ src/app/main-editor/main-editor.component.ts
@@ -18,7 +18,38 @@ export class MainEditorComponent {
 
   constructor(public cfg: ConfigService) {}
 
-  onViewPortOrigin(pt: { x: number; y: number }) {
+  onZoomChange({
+    deltaX,
+    deltaY,
+    ptX,
+    ptY,
+  }: {
+    deltaX: number;
+    deltaY: number;
+    ptX: number;
+    ptY: number;
+  }) {
+    const MAX_DELTA = 10;
+    let delta = Math.min(Math.abs(deltaY), MAX_DELTA);
+    const sign = -Math.sign(deltaY);
+    delta *= sign;
+
+    const oldZoom = round(this.cfg.zoom);
+    const newZoom = oldZoom * (1 + delta / 100);
+
+    // ptXY should stay on the same device-Coord
+    // (ptX - oldViewX) * oldZoom ==  (ptX - newViewX) * newZoom
+    //
+    const newX = ptX - (oldZoom / newZoom) * (ptX - this.cfg.viewPortX);
+    const newY = ptY - (oldZoom / newZoom) * (ptY - this.cfg.viewPortY);
+
+    this.cfg.viewPortX = round(newX);
+    this.cfg.viewPortY = round(newY);
+    this.cfg.zoom = round(newZoom);
+    this.recalcViewPort();
+  }
+
+  onViewPortChange(pt: { x: number; y: number }) {
     this.cfg.viewPortX = round(pt.x);
     this.cfg.viewPortY = round(pt.y);
   }
@@ -33,19 +64,6 @@ export class MainEditorComponent {
   recalcViewPort() {
     this.cfg.viewPortWidth = round(this.canvasWidth / this.cfg.zoom);
     this.cfg.viewPortHeight = round(this.canvasHeight / this.cfg.zoom);
-  }
-
-  onZoomChange({ deltaX, deltaY }: { deltaX: number; deltaY: number }) {
-    const MAX_DELTA = 10;
-    let delta = Math.min(Math.abs(deltaY), MAX_DELTA);
-    const sign = -Math.sign(deltaY);
-    delta *= sign;
-
-    const oldZoom = round(this.cfg.zoom);
-    const newZoom = oldZoom * (1 + delta / 100);
-
-    this.cfg.zoom = round(newZoom);
-    this.recalcViewPort();
   }
 
   getViewport() {
